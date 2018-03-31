0fdf06917f255832da7eeaf8f1f1d64464554105
sarjumulmi <mulmi.sarju@gmail.com>
2018-03-31 11:33:34 -0400

    getDomains route and action setup. data received from api endpoint.

--- controllers/domainController.js
+++ controllers/domainController.js
@@ -1,5 +1,22 @@
+const axios = require('axios');
+
 const API_KEY = 'c06e7c44f16cc2dcd59f33ee4709ac78';
+const BASE_URL = 'https://domainscope.com/api/v2/domains/nxd?keyword=';
+
 
 exports.index = (req, res) => {
   res.render('home', {title: 'Domain Checker'});
+}
+
+exports.getDomains =  (req, res) => {
+  const query = req.query.domain;
+  const url = `${BASE_URL}${query}&page_size=10`;
+  axios.get(url,
+    {headers: {'X-DOMAINSCOPE-APIKEY': `${API_KEY}`}}
+  ).then(response => {
+    console.log(response.data);
+    res.json({url:url});
+  })
+
+
 }
--- routes.js
+++ routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = require('./app');
-const homeController = require('./controllers/homeController');
+const domainController = require('./controllers/domainController');
 
 const router = express.Router();
 router.get('/', domainController.index);
+router.get('/getDomains*', domainController.getDomains)
 
 module.exports = router;
