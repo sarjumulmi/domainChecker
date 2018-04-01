58e741897760475ce0cffa9024b81a14e83b3933
sarjumulmi <mulmi.sarju@gmail.com>
2018-04-01 13:29:53 -0400

    nav bar added. domainList mixin view updated.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
